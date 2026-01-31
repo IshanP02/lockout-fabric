344e1e45616e7e5841748465bba64ea995ab2d60
Coreybirdo <coreybird12@gmail.com>
2026-01-31 06:13:31 -0500

    adjusted item stack for Obtain4UniqueSeeds to make it more aligned with other similar goals

--- src/main/java/me/marin/lockout/lockout/goals/obtain/Obtain4UniqueSeedsGoal.java
+++ src/main/java/me/marin/lockout/lockout/goals/obtain/Obtain4UniqueSeedsGoal.java
@@ -1,7 +1,10 @@
 package me.marin.lockout.lockout.goals.obtain;
 
 import me.marin.lockout.lockout.interfaces.ObtainSomeOfTheItemsGoal;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawContext;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 
 import java.util.List;
@@ -9,6 +12,7 @@ import java.util.List;
 public class Obtain4UniqueSeedsGoal extends ObtainSomeOfTheItemsGoal {
 
     private static final List<Item> ITEMS = List.of(Items.WHEAT_SEEDS, Items.BEETROOT_SEEDS, Items.MELON_SEEDS, Items.PUMPKIN_SEEDS, Items.TORCHFLOWER_SEEDS);
+    private static final ItemStack ITEM_STACK = Items.WHEAT_SEEDS.getDefaultStack();
 
     public Obtain4UniqueSeedsGoal(String id, String data) {
         super(id, data);
@@ -27,5 +31,12 @@ public class Obtain4UniqueSeedsGoal extends ObtainSomeOfTheItemsGoal {
     @Override
     public int getAmount() {
         return 4;
+    }
+
+    @Override
+    public boolean renderTexture(DrawContext context, int x, int y, int tick) {
+        super.renderTexture(context, x, y, tick);
+        context.drawStackOverlay(MinecraftClient.getInstance().textRenderer,  ITEM_STACK, x, y, "4");
+        return true;
     }
 }
